ffcf8418966ee0f15599b12e6ca77c2f77d327fe
Rinal Shastri <rinal.shastri@outlook.com>
2024-08-23 10:53:21 -0700

    mental health comments

--- server/mental_health_app.py
+++ server/mental_health_app.py
@@ -2,6 +2,9 @@ from flask import Flask, jsonify
 import pandas as pd
 import sqlite3
 from flask_cors import CORS
+from wordcloud import STOPWORDS
+from collections import Counter
+import re
 
 app = Flask(__name__)
 
@@ -25,8 +28,59 @@ def get_tech_workers_data():
     result = result_df.to_dict(orient='records')
     return jsonify(result)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+# API endpoint
+@app.route('/mental-vs-physical', methods=['GET'])
+def get_mental_vs_physical():
+    conn = sqlite3.connect('./survey_database.db')
+
+    query = f"SELECT mental_vs_physical FROM survey_data WHERE Country = 'United States';"
+    result_df = pd.read_sql_query(query, conn)
+
+    conn.close()
+
+    # Get the data as JSON
+    result = result_df.to_dict(orient='records')
+    return jsonify(result)
+
+# API endpoint
+@app.route('/mental-health-comments', methods=['GET'])
+def get_mental_health_comments():
+    conn = sqlite3.connect('./survey_database.db')
+
+    query = f"SELECT comments FROM survey_data WHERE Country = 'United States';"
+    result_df = pd.read_sql_query(query, conn)
+
+    conn.close()
+
+    # Drop rows with None or NaN values in the comments column
+    usa_comments_df = result_df.dropna(subset=['comments'])
+
+    # Combine and clean all comments into a single string
+    all_comments = ' '.join(usa_comments_df['comments'].dropna().str.lower())
+
+    # Remove punctuation and split text into words
+    words = re.findall(r'\b\w+\b', all_comments)
+
+    # Remove common stopwords and define relevant keywords
+    stopwords = set(STOPWORDS)
+    filtered_words = [word for word in words if word not in stopwords]
+    tts_keywords = ['therapy', 'treatment', 'support']
+    mental_health_keywords = ['depression', 'anxiety', 'stress'] + ['tts']
+
+    # Replace 'therapy', 'treatment', and 'support' with 'tts'
+    filtered_words = ['tts' if word in tts_keywords else word for word in filtered_words]
+
+    # Count the occurrences of relevant mental health words
+    word_counts = Counter(word for word in filtered_words if word in mental_health_keywords)
+
+    # Convert the counts to a DataFrame
+    word_counts_df = pd.DataFrame(word_counts.items(), columns=['Word', 'Frequency']).sort_values(by='Frequency', ascending=False)
+
+    # Get the data as JSON
+    result = word_counts_df.to_dict(orient='records')
+
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
